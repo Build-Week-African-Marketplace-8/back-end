a8927bfe4e3e163d6142883739885d0279332df0
Kenji Greene <kenjigrr@gmail.com>
2021-12-20 03:27:12 -0600

    connected database to postgres and added middleware

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const routerUsers = require("./users/router");
+const routerProfiles = require("./profiles/router");
+const routerProducts = require("./products/router");
+const routerAuth = require("./auth/router");
+const {errorHandler} = require("./middleware/errorHandle");
+const {authorizedOnly} = require("./auth/middleware");
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get("/", (req, res)=>{
+    res.status(200).json({message:"hello world from African Marketplace Dec 2021"});
+})
+server.use("/api/users", authorizedOnly, routerUsers);
+server.use("/api/profiles", authorizedOnly, routerProfiles);
+server.use("/api/products", routerProducts);
+server.use("/api/auth", routerAuth);
+
+server.use('*', (req, res)=>{
+    res.status(404).json({message:`invalid path ${req.path}`});
+})
+
+server.use(errorHandler);
+
+module.exports = server;
